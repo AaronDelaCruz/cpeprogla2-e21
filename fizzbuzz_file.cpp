386723b24006d442ff3684b87643efb46b92b990
AaronDelaCruz <aarondelacruz45@gmail.com>
2016-10-17 08:26:44 +0800

    Create fizzbuzz_file.cpp

---
+++ fizzbuzz_file.cpp
@@ -0,0 +1,22 @@
+//Dela Cruz, Aaron Christian C.
+//CPEPROG2LA E21
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	ifstream f("input.txt");
+	int a;
+	while(f>>a){
+		if(a%3==0&&a%5==0)
+		cout<< "FizzBuzz" <<endl;
+		else if(a%3==0)
+		cout<< "Fizz" <<endl;
+	 	else if(a%5==0)
+		cout<< "Buzz" <<endl;
+		else 
+		cout<< a<<endl;
+	}
+	return 0;
+}
